82bd76576726ea4e6c668b098957c19d139d5ece
Sanjalionething <sanjalisahu@onething.design>
2024-01-23 13:13:53 +0530

    remove redux

--- essentials/redux/slice/ContactSlice.js
+++ essentials/redux/slice/ContactSlice.js
@@ -1,14 +1,14 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const contactSlice = createSlice({
-  name: "contact-form",
-  initialState: [],
-  reducers: {
-    // getContact(state, action) {
-    //   return action.payload;
-    // },
-  },
-});
+// const contactSlice = createSlice({
+//   name: "contact-form",
+//   initialState: [],
+//   reducers: {
+//     // getContact(state, action) {
+//     //   return action.payload;
+//     // },
+//   },
+// });
 
-// export const { getContact } = contactSlice.actions;
-export default contactSlice.reducer;
+// // export const { getContact } = contactSlice.actions;
+// export default contactSlice.reducer;
--- essentials/redux/store.js
+++ essentials/redux/store.js
@@ -1,25 +1,25 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import contactReducer from "../redux/slice/ContactSlice";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from "redux-persist";
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import contactReducer from "../redux/slice/ContactSlice";
+// import storage from "redux-persist/lib/storage";
+// import { persistReducer, persistStore } from "redux-persist";
 
-const persistConfig = {
-  key: "root",
-  storage,
-};
-const rootReducer = combineReducers({
-  contact: contactReducer,
+// const persistConfig = {
+//   key: "root",
+//   storage,
+// };
+// const rootReducer = combineReducers({
+//   contact: contactReducer,
 
-  //more reducers
-});
+//   //more reducers
+// });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
-});
-export const persistor = persistStore(store);
+// export const store = configureStore({
+//   reducer: persistedReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       serializableCheck: false,
+//     }),
+// });
+// export const persistor = persistStore(store);
